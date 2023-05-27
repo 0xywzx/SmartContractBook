aded9b961ac770cdf422c881335d4504a20b4242
0xywzx <gnx.vw903@gmail.com>
2023-05-27 20:31:56 +0900

    fix front error

--- x.x.nft/front/app/page.tsx
+++ x.x.nft/front/app/page.tsx
@@ -18,12 +18,14 @@ export default function Home() {
   const [nftImage, setNftImage] = useState("");
   const [rarity, setRarity] = useState("");
   const [content, setContent] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
 
   const fetchNFT = async () => {
     if (!address || !chain || !isSupportedChain(chain.id)) return
 
     const contract = getContract({
-      address: getContractAddress(chain.id),
+      address: `0x${getContractAddress(chain.id).slice(2)}`,
       abi: abi.abi,
       chainId: chain?.id,
     })
@@ -52,7 +54,11 @@ export default function Home() {
       setRarity(data.attributes[0].value);
       setNftImage(data.image);
     })
-  }, [address, chain])
+    // there was an error due to discrepancies between the client and server-side when using isConnected
+    if (isConnected) {
+      setIsWalletConnected(true)
+    }
+  }, [address, chain, isConnected])
 
   const handleSwitchChain = async () => {
     console.log('handleSwitchChain');
@@ -81,54 +87,57 @@ export default function Home() {
       <h1 className="text-3xl font-bold mb-8">Smart Contract Book</h1>
 
       <ConnectButton accountStatus="avatar" />
+        {isWalletConnected ? (
+          <>
+            {chain && !isSupportedChain(chain.id) && (
+              <div className="flex flex-col items-center justify-center mt-4">
+                <p>Please switch to the desired chain to access the content.</p>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+                  onClick={handleSwitchChain}
+                >
+                  Switch Chain
+                </button>
+              </div>
+            )}
 
-      {isConnected ? (
-        <>
-          {chain && !isSupportedChain(chain.id) && (
-            <div className="flex flex-col items-center justify-center mt-4">
-              <p>Please switch to the desired chain to access the content.</p>
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
-                onClick={handleSwitchChain}
-              >
-                Switch Chain
-              </button>
-            </div>
-          )}
+            {nftImage ? (
+              <>
+                <div className="flex flex-col items-center justify-center">
+                  <div className="py-4" style={{ maxWidth: '400px' }}>
+                    <img src={nftImage} alt="Image" className="max-w-full" />
+                  </div>
+                  <div className="mb-4">
+                    <p>TokenID: {tokenId}</p>
+                    <p>Rarity: {rarity}</p>
+                  </div>
+                </div>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+                  onClick={handleSignMessage}
+                >
+                  Access Content
+                </button>
 
-          {nftImage ? (
+                <div className="mt-8">
+                  {content}
+                </div>
+              </>
+            ) : (
+              <div className="flex flex-col items-center justify-center">
+                <p>This address does not own an NFT.</p>
+              </div>
+            )}
+          </>
+          ) : (
             <>
               <div className="flex flex-col items-center justify-center">
-                <div className="py-4" style={{ maxWidth: '400px' }}>
-                  <img src={nftImage} alt="Image" className="max-w-full" />
-                </div>
-                <div className="mb-4">
-                  <p>TokenID: {tokenId}</p>
-                  <p>Rarity: {rarity}</p>
-                </div>
-              </div>
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-                onClick={handleSignMessage}
-              >
-                Access Content
-              </button>
-
-              <div className="mt-8">
-                {content}
+                <p>Please connect your account to access the content.</p>
               </div>
             </>
-          ) : (
-            <div className="flex flex-col items-center justify-center">
-              <p>This address does not own an NFT.</p>
-            </div>
           )}
-        </>
-      ) : (
-        <div className="flex flex-col items-center justify-center">
-          <p>Please connect your account to access the content.</p>
-        </div>
-      )}
+
     </main>
   )
 }
+
